acc637d8f3e9955eab28d7f337a5485399cce8f4
ThomasLee94 <thomaslee9474@gmail.com>
2025-08-04 16:45:42 -0700

    rm ports

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nclusion-takehome",
-  preview: {
-    port: 4173,
-    host: '0.0.0.0'
-  }
+  base: "/nclusion-takehome"
 })
